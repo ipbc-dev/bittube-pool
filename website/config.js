5914dbba1dedbbf81b8caf12820eebf1f0ad5ab6
s.skomudek <s.skomudek@devcups.com>
2018-01-09 14:27:16 +0000

    init

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "https://pool.ipbc.io:8119";
+
+var api_blockexplorer = "http://pool.ipbc.io:24182";
+
+var poolHost = "pool.ipbc.io";
+
+var irc = "irc.freenode.net/#ipbc";
+
+var email = "hello@ipbc.io";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "/?hash={id}#blockchain_block";
+
+var transactionExplorer = "/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "ipbc": [
+        ["pool.ipbc.io", "https://pool.ipbc.io:8119"]
+    ]
+};
